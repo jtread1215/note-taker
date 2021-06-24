5415bafc96fa185aa829b318e15c55c5444d4ac3
jtread1215 <77247419+jtread1215@users.noreply.github.com>
2021-06-23 20:19:34 -0400

    update

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3000;
 //Routes and middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(("public")),
+app.use(express.static("public"));
 app.use("/api", apiRoutes),
 app.use("/", htmlRoutes),
 
 app.listen(PORT, () => {
     console.log(`Server is listening port: ${PORT}`)
-}));
+});
